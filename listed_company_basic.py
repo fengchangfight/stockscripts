0252a6a92bc3b7dcd1a6a6df5f3a2c2064a34c2e
fengchang <fengchang_fight@163.com>
2020-12-20 11:47:43 +0800

    Update listed_company_basic.py

--- listed_company_basic.py
+++ listed_company_basic.py
@@ -3,7 +3,14 @@ import tushare as ts
 token="****"
 ts.set_token(token)
 pro = ts.pro_api()
-data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-# output to excel of type <class 'pandas.core.frame.DataFrame'>
-data.to_excel("/tmp/stockbasic_output.xlsx") 
-
+data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,name,area,industry,list_date')
+col_names=['ts_code','name','area','industry','list_date']
+col_map={
+    "ts_code":"代码",
+"name":"名称",
+"area":"地区",
+"industry":"行业",
+"list_date":"上市日期"
+}
+cust_header = [ col_map[x] for x in col_names]
+data.to_excel("/Users/xiefengchang/life/stockbasic_info.xlsx", header=cust_header, index=False) 
