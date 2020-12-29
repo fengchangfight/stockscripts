dd20251381c14867d65aba8cd3b736a58a88e3ff
fengchang <fengchang_fight@163.com>
2020-12-29 22:12:58 +0800

    Update listed_company_basic.py

--- listed_company_basic.py
+++ listed_company_basic.py
@@ -1,16 +1,39 @@
 import tushare as ts
+import datetime
+from datetime import date
+import pandas as pd
 
-token="****"
+
+def formatDate(dt):
+    format = "%Y%m%d"
+    return dt.strftime(format)
+
+
+def normalize_date2weekday(dt):
+    _, _, day_of_week = dt.isocalendar()
+    if(day_of_week == 6):
+        return dt - datetime.timedelta(days=1)
+    elif(day_of_week == 7):
+        return dt - datetime.timedelta(days=2)
+    else:
+        return dt
+
+
+todayStr = formatDate(normalize_date2weekday(date.today()))
+
+token = "***"
 ts.set_token(token)
 pro = ts.pro_api()
-data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,name,area,industry,list_date')
-col_names=['ts_code','name','area','industry','list_date']
-col_map={
-    "ts_code":"代码",
-"name":"名称",
-"area":"地区",
-"industry":"行业",
-"list_date":"上市日期"
+data = pro.stock_basic(exchange='', list_status='L',
+                       fields='ts_code,name,area,industry,list_date')
+col_names = ['ts_code', 'name', 'area', 'industry', 'list_date']
+col_map = {
+    "ts_code": "代码",
+    "name": "名称",
+    "area": "地区",
+    "industry": "行业",
+    "list_date": "上市日期"
 }
-cust_header = [ col_map[x] for x in col_names]
-data.to_excel("/Users/xiefengchang/life/stockbasic_info.xlsx", header=cust_header, index=False) 
+cust_header = [col_map[x] for x in col_names]
+data.to_excel("/Users/xiefengchang/life/stockbasic_info_"+todayStr+".xlsx",
+              header=cust_header, index=False)
