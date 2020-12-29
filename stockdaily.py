00299476f4ccffeec9cf5e6c1deae25162e5cc9d
fengchang <fengchang_fight@163.com>
2020-12-29 22:03:34 +0800

    Update stockdaily.py

--- stockdaily.py
+++ stockdaily.py
@@ -3,26 +3,14 @@ import datetime
 from datetime import date
 import pandas as pd
 
-basic_info_file = '/Users/xiefengchang/life/stockbasic_info.xlsx'
-
-
-def get_code_map_from_basic_info():
-    result_dict = {}
-    excel_data_df = pd.read_excel(basic_info_file, sheet_name='Sheet1')
-    df = pd.DataFrame(excel_data_df, columns=['代码', '名称', '地区', '行业', '上市日期'])
-    for index, row in df.iterrows():
-        code = row['代码']
-        result_dict[code] = row
-    return result_dict
-
 
 def formatDate(dt):
     format = "%Y%m%d"
     return dt.strftime(format)
 
 
-def normalizeDate(dt):
-    year, week_num, day_of_week = dt.isocalendar()
+def normalize2weekday(dt):
+    _, _, day_of_week = dt.isocalendar()
     if(day_of_week == 6):
         return dt - datetime.timedelta(days=1)
     elif(day_of_week == 7):
@@ -31,14 +19,41 @@ def normalizeDate(dt):
         return dt
 
 
-todayStr = formatDate(normalizeDate(date.today()))
+todayStr = formatDate(normalize2weekday(date.today()))
+basic_info_file = '/Users/xiefengchang/life/stockbasic_info_'+todayStr+'.xlsx'
+stock_bak_info = '/Users/xiefengchang/life/stockdaily_bak_output_'+todayStr+'.xlsx'
+# yesterdayStr = formatDate(normalize2weekday(
+#     date.today() - datetime.timedelta(days=1)))
 
+date_str = todayStr
 token = "c0b191a58ac95478267a597abbcf656bec52aa9ec52afa3d1228bfbf"
 ts.set_token(token)
-
-
 pro = ts.pro_api()
-df = pro.daily_basic(ts_code='', trade_date=todayStr,
+
+
+def get_code_map_from_basic_info():
+    result_dict = {}
+    excel_data_df = pd.read_excel(basic_info_file, sheet_name='Sheet1')
+    df = pd.DataFrame(excel_data_df, columns=['代码', '名称', '地区', '行业', '上市日期'])
+    for _, row in df.iterrows():
+        code = row['代码']
+        result_dict[code] = row
+    return result_dict
+
+
+def get_stock_bak_dict():
+    result_dict = {}
+    excel_data_df = pd.read_excel(stock_bak_info, sheet_name='Sheet1')
+    df = pd.DataFrame(excel_data_df, columns=['代码',
+                                              '交易日', '内盘', '外盘', '开盘价', '收盘价', '最高价', '最低价', '总市值', '强弱度', '活跃度'])
+    for _, row in df.iterrows():
+        code = row['代码']
+        result_dict[code] = row
+    return result_dict
+
+
+print("the base date used is:{0}".format(date_str))
+df = pro.daily_basic(ts_code='', trade_date=date_str,
                      fields='ts_code,trade_date,total_mv,volume_ratio,pe,pe_ttm,pb,dv_ratio,total_share,turnover_rate,close')
 
 name_map = {'ts_code': '代码',
@@ -53,14 +68,18 @@ name_map = {'ts_code': '代码',
             'turnover_rate': '换手率',
             'name': '名称',
             'close': '当日收盘价',
-            'industry': '行业'
+            'industry': '行业',
+            'selling': '内盘'
             }
 col_order = ['ts_code', 'name', 'industry', 'trade_date', 'total_mv',
-             'volume_ratio', 'pe', 'pe_ttm', 'pb', 'dv_ratio', 'total_share', 'turnover_rate', 'close']
+             'volume_ratio', 'pe', 'pe_ttm', 'pb', 'dv_ratio', 'total_share', 'turnover_rate', 'close', 'selling']
 custom_header = [name_map[r] for r in col_order]
 basic_dict = get_code_map_from_basic_info()
+bak_dict = get_stock_bak_dict()
+
 #df.columns = ['代码','交易日期','总市值','量比','市盈率','市净率','换手率']
 df['name'] = df.apply(lambda row: basic_dict[row.ts_code]['名称'], axis=1)
 df['industry'] = df.apply(lambda row: basic_dict[row.ts_code]['行业'], axis=1)
-df.to_excel("/Users/xiefengchang/life/stockdaily_output_"+todayStr +
+df['selling'] = df.apply(lambda row: bak_dict[row.ts_code]['内盘'], axis=1)
+df.to_excel("/Users/xiefengchang/life/stockdaily_merged_output_"+date_str +
             ".xlsx", index=False, columns=col_order, header=custom_header)
