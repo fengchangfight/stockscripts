8b2570e4f87c585c8f5112e39c5e2e37de81ebbe
fengchang <fengchang_fight@163.com>
2021-01-02 14:49:36 +0800

    Update stockdaily.py

--- stockdaily.py
+++ stockdaily.py
@@ -1,8 +1,11 @@
+# 每日股票信息大全
+
 import tushare as ts
 import datetime
 from datetime import date
 import pandas as pd
-
+import sys
+from collections import defaultdict
 # 本文件依赖stock_basic_info.py和stock_bak_daily.py的产出文件作为输入
 
 
@@ -22,19 +25,30 @@ def normalize2weekday(dt):
 
 
 todayStr = formatDate(normalize2weekday(date.today()))
+if(len(sys.argv) > 1 and sys.argv[1] != None):
+    todayStr = sys.argv[1]
+
 basic_info_file = '/Users/xiefengchang/life/stockbasic_info_'+todayStr+'.xlsx'
 stock_bak_info = '/Users/xiefengchang/life/stockdaily_bak_output_'+todayStr+'.xlsx'
 # yesterdayStr = formatDate(normalize2weekday(
 #     date.today() - datetime.timedelta(days=1)))
 
 date_str = todayStr
-token = "***"
+token = "c0b191a58ac95478267a597abbcf656bec52aa9ec52afa3d1228bfbf"
 ts.set_token(token)
 pro = ts.pro_api()
 
 
+def inner_def_value():
+    return ''
+
+
+def def_value():
+    return defaultdict(inner_def_value)
+
+
 def get_code_map_from_basic_info():
-    result_dict = {}
+    result_dict = defaultdict(def_value)
     excel_data_df = pd.read_excel(basic_info_file, sheet_name='Sheet1')
     df = pd.DataFrame(excel_data_df, columns=['代码', '名称', '地区', '行业', '上市日期'])
     for _, row in df.iterrows():
@@ -44,7 +58,7 @@ def get_code_map_from_basic_info():
 
 
 def get_stock_bak_dict():
-    result_dict = {}
+    result_dict = defaultdict(def_value)
     excel_data_df = pd.read_excel(stock_bak_info, sheet_name='Sheet1')
     df = pd.DataFrame(excel_data_df, columns=['代码',
                                               '内盘', '外盘', '开盘价', '最高价', '最低价', '总市值', '强弱度', '活跃度'])
@@ -84,14 +98,27 @@ custom_header = [name_map[r] for r in col_order]
 basic_dict = get_code_map_from_basic_info()
 bak_dict = get_stock_bak_dict()
 
+
+def row_property_extractor(row_dict, ts_code, property_name):
+    return row_dict[ts_code][property_name]
+
+
 #df.columns = ['代码','交易日期','总市值','量比','市盈率','市净率','换手率']
-df['name'] = df.apply(lambda row: basic_dict[row.ts_code]['名称'], axis=1)
-df['industry'] = df.apply(lambda row: basic_dict[row.ts_code]['行业'], axis=1)
-df['selling'] = df.apply(lambda row: bak_dict[row.ts_code]['内盘'], axis=1)
-df['open'] = df.apply(lambda row: bak_dict[row.ts_code]['开盘价'], axis=1)
-df['low'] = df.apply(lambda row: bak_dict[row.ts_code]['最低价'], axis=1)
-df['high'] = df.apply(lambda row: bak_dict[row.ts_code]['最高价'], axis=1)
-df['strength'] = df.apply(lambda row: bak_dict[row.ts_code]['强弱度'], axis=1)
-df['activity'] = df.apply(lambda row: bak_dict[row.ts_code]['活跃度'], axis=1)
+df['name'] = df.apply(lambda row: row_property_extractor(
+    basic_dict, row.ts_code, '名称'), axis=1)
+df['industry'] = df.apply(lambda row: row_property_extractor(
+    basic_dict, row.ts_code, '行业'), axis=1)
+df['selling'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '内盘'), axis=1)
+df['open'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '开盘价'), axis=1)
+df['low'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '最低价'), axis=1)
+df['high'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '最高价'), axis=1)
+df['strength'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '强弱度'), axis=1)
+df['activity'] = df.apply(lambda row: row_property_extractor(
+    bak_dict, row.ts_code, '活跃度'), axis=1)
 df.to_excel("/Users/xiefengchang/life/stockdaily_merged_output_"+date_str +
             ".xlsx", index=False, columns=col_order, header=custom_header)
